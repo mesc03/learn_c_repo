6dafb7af75c31f04a6260c314b676f81ed89a1d9
Michael Patrick <122712401+mesc03@users.noreply.github.com>
2025-12-06 13:07:28 +0000

    deleted the comment and moved it above

--- practice_1.c
+++ practice_1.c
@@ -1,4 +1,7 @@
 // this program is to a print function
+// This program prints a greeting message with the name "Michael Patrick Reyes".
+// To run the compile the program, start with "gcc <file name> -o hello then execute "./hello" in the terminal.
+// another way is to just do gcc <file name> -o <file name> and then ./<file name> in the terminal
 #include <stdio.h>
 
 int main() {
@@ -6,7 +9,3 @@ int main() {
     printf("Hello %s", name);
     return 0;
 }
-
-// This program prints a greeting message with the name "Michael Patrick Reyes".
-// To run the compile the program, start with "gcc <file name> -o hello then execute "./hello" in the terminal.
-// another way is to just do gcc <file name> -o <file name> and then ./<file name> in the terminal
