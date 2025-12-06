bb877d5189ddb26b5c91850e3383e2f9d28615e1
Michael Patrick <122712401+mesc03@users.noreply.github.com>
2025-12-06 13:26:23 +0000

    Enhance practice_2.c with additional print statements and comments for clarity

--- practice_2.c
+++ practice_2.c
@@ -3,7 +3,15 @@
 int main() {
     char name[] = "Michael Patrick Reyes";
     printf("Hello %s", name);
-    printf("\nCongrats in learning c programming!");
+    printf("\nCongrats in learning c programming!"); // when printing this it produces a new line before the text
     printf("\nEnjoy the learning");
+    printf("\nThis line when print\t, produces space");
+    printf("\nThis line produces a backslash after this text\\");
+    printf("\nThis line adds a double quote after this text\"");
     return 0;
 }
+
+// "\t" adds a space between the text
+// "\\" adds a single \ after the text
+// "\"" adds a double quote after the text
+// "\n" adds a new line after the text
