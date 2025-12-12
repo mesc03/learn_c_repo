a45b681fbf5957c6b72fbba090421b15efaf98a0
Michael Patrick <122712401+mesc03@users.noreply.github.com>
2025-12-12 06:14:56 +0000

    refactor practice_4.c: uncomment main functions and add age output example

--- practice_4.c
+++ practice_4.c
@@ -1,38 +1,50 @@
 #include <stdio.h>
-int main() {
+/* int main() {
     int myNum = 15; 
     int myOtherNum = 10;
     myNum = myOtherNum; // meaning my number wil become 10 as it follows the othernum function
     printf("%d", myNum); // this will now print 10 since i declared myNum to be othernum which is 10
     return 0;
-}
+} */
 // compile it first using "gcc practice_X.c -o practice_X ./practice_X"
 // X is the number of the practice file
 // and then after, run it by "gcc practice_X.c -o practice_X" or "./practice_X"
 
 #include <stdio.h>
-int main() {
+/* int main() {
     int x = 11;
     int y = 11;
     int sum  = x + y; // output would be 22
     printf("This is how old i am %d", sum);
-}
+} */
 
 // if there are any changes to the code always compile it first before running it again.
 // gcc practice_X.c -o practice_X and then do ./practice_X 
 
 #include <stdio.h>
-int main() {
+/* int main() {
     int x = 10, y = 10, z = 1;
     printf("My true age would be %d\n", x + y + z);
     return 0;
-} 
+} */
 
 // tested multiple variable declaration in one line
 #include <stdio.h>
-int main() {
+/* int main() {
     int x, y, z;
     x = y = z = 10;
     printf("I would be too old if i told you my age was %d\n", x + y + z);
     return 0;
-} 
+} */
+
+// variable names
+#include <stdio.h>
+
+int main() {
+  int ageThisYear = 60;
+  int a = 60;
+  
+  printf("The age i would want to be at to retire: %d\n", ageThisYear);
+  printf("A shortcut varaiable produced this number: %d\n", a);
+  return 0;
+}
